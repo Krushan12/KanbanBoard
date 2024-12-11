0c5bdb43411e2cb2623302e9cdf13e52d186e1a1
Krushant Wagh <krudhantw@gmail.com>
2024-12-12 01:19:42 +0530

    Add frontend folder

---
+++ frontend/src/components/Task.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react"
+import { use } from "react"
+
+export default function KanbanBoard(){
+    const [complete,setComplete] = useState([]);
+    const [inComplete, setInComplete] = useState([]);
+
+    return (
+        <div>
+
+        </div>
+    )
+}
